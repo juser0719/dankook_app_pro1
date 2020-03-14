e0f018696c7bf65d55e928ef13029e2cf55c1c91
juser0719 <sinsa0719@naver.com>
2020-03-14 18:15:36 +0900

    log_in, sign_in, design

---
+++ app/src/main/java/com/example/woong/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.example.woong;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            Thread.sleep(1000); //대기 초 설정
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
+    }
+}
